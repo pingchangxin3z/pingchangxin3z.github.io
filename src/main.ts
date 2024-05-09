365b85b8ba7cc6214a6b503dfd53dec2946392ea
zhangzuzhou <1760189000@qq.com>
2024-05-09 18:51:56 +0800

    fixxx

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,7 @@ import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="../public/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="./typescript.svg" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <a href="../public/test.jpg" download>下载图片</a>
   </div>
 `
 
