c8fcf95298df54bd5964e6d32eb044a092499f24
zhangzuzhou <1760189000@qq.com>
2024-05-10 10:37:37 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { setupCounter } from './counter'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div>1</div>
-    <a href="https://idox6kp.sufydely.com/test.jpg" download>下载图片</a>
+    <a href="/test.jpg" download>下载图片</a>
   </div>
 `
 
