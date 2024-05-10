aee701bb70258fa7090b9b93c726ef44d1aaa638
zhangzuzhou <1760189000@qq.com>
2024-05-10 13:34:52 +0800

    change

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <div>1</div>
+    <div>3</div>
     <a href="/test.jpg" download>下载图片</a>
   </div>
 `
