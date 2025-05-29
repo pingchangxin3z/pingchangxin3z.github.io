773b955c164336642dae98efcd7f94b175160f59
zzz <pingchangxin3z@163.com>
2025-05-29 11:48:41 +0800

    fix

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>老浮该吃饭啦</h1>
+    </div>
+  )
+}
+
+export default App
